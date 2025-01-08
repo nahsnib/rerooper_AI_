c31ea61a09ca2e62467b7a2b9c6ead44ea6a5429
nahsnib <hsnubinshan@gmail.com>
2025-01-08 09:17:59 +0800

    Update RuleTable.py

--- datebase/RuleTable.py
+++ datebase/RuleTable.py
@@ -114,7 +114,7 @@ ruleTable.addMainRule(ancientMyth);
 ruleTable.addSubRule(causalLine);
 
 // 顯示所有規則
-ruleTable.loadplayRules();
+ruleTable.load_RuleTable();
 // 定義主要規則表 Basic Tragedy X
 const BasicTragedyX = {
     name: "Basic Tragedy X",
