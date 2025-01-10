35708f1fee29bbc0cad69ae3308411f613c5e837
nahsnib <hsnubinshan@gmail.com>
2025-01-10 11:22:03 +0800

    Update night_phase.py

--- game_phases/night_phase.py
+++ game_phases/night_phase.py
@@ -1,9 +1,17 @@
 
-# game_phases/night_phase.py
-
+# game_phase/night_phase.py
 class NightPhase:
-    def __init__(self, board):
-        self.board = board
+    def __init__(self, character_manager):
+        self.character_manager = character_manager
+
+    def execute(self):
+        # 執行夜間階段的邏輯
+        self.reset_all_abilities()
+
+    def reset_all_abilities(self):
+        for character in self.character_manager.characters:
+            character.reset_ability_usage()
+
 
     def execute(self):
         print("執行夜晚階段...")
