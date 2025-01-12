13b10ce25fc586ce93299dac09623af87f4cbb5e
nahsnib <hsnubinshan@gmail.com>
2025-01-12 20:47:29 +0800

    Update board.py

--- common/board.py
+++ common/board.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from datetime import datetime
-from game import GameLoop
 
 class Area:
     def __init__(self, id, name):
@@ -30,7 +28,6 @@ class Area:
         print("角色:")
         for character in self.characters:
             print(f"  - {character.name}")
-
 
 # 定義地區
 school = Area(1, "學校")
@@ -98,27 +95,3 @@ class GameBoard:
         self.total_days_label.config(text=str(self.game.total_days))
         self.current_day_label.config(text=str(self.game.day_counter))
         self.update_events()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("遊戲版面")
-
-    character_manager = CharacterManager()
-    role = "偵探"
-    total_days = 30
-    scheduled_events = {
-        3: "事件A",
-        7: "事件B",
-        15: "事件C"
-    }
-
-    game = GameLoop(character_manager, role, total_days, scheduled_events)
-    game_board = GameBoard(root, game)
-
-    def update_game_board():
-        game.increment_day()
-        game_board.update()
-        root.after(1000, update_game_board)
-
-    root.after(1000, update_game_board)
-    root.mainloop()
