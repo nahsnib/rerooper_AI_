58a08ce4042274ff029d13772b2bbbd986bacaaf
nahsnib <hsnubinshan@gmail.com>
2025-01-13 10:56:14 +0800

    Update ai_gameset.py

--- scriptwriter/ai_gameset.py
+++ scriptwriter/ai_gameset.py
@@ -10,12 +10,11 @@ class AIGameSet:
         self.initialize_script()
 
     def load_rule_tables(self):
-        # 假設這裡讀取並返回所有規則表
         return {
-            1: RuleTable(1, "Basic Tragedy X"),
-            2: RuleTable(2, "Basic Tragedy Y"),
-            3: RuleTable(3, "Basic Tragedy Z"),
-            4: RuleTable(4, "Basic Tragedy W")
+            1: get_rule_table_by_id(1),
+            2: get_rule_table_by_id(2),
+            3: get_rule_table_by_id(3),
+            4: get_rule_table_by_id(4)
         }
 
     def initialize_script(self):
@@ -45,12 +44,10 @@ class AIGameSet:
         self.event_criminals = self.assign_event_criminals()
 
     def select_main_rule_table(self):
-        # 假設我們隨機選擇一個規則表
         rule_table_id = random.choice(list(self.rule_tables.keys()))
-        return get_rule_table_by_id(rule_table_id)
+        return self.rule_tables[rule_table_id]
 
     def select_characters(self, min_characters, max_characters):
-        # 假設我們隨機選擇角色數量並從角色資料庫中選擇角色
         num_characters = random.randint(min_characters, max_characters)
         return self.character_db.select_characters(num_characters)
 
@@ -64,48 +61,4 @@ class AIGameSet:
         events = random.sample(self.main_rule_table.events, k=self.total_days)
         scheduled_events = {}
         available_days = list(range(1, self.total_days + 1))
-        random.shuffle(available_days)
-        for event in events:
-            if available_days:
-                day = available_days.pop()
-                scheduled_events[day] = event
-        return scheduled_events
-
-    def select_main_rule(self):
-        return random.choice(self.main_rule_table.main_rules)
-
-    def select_sub_rules(self, num_sub_rules):
-        return random.sample(self.main_rule_table.sub_rules, k=num_sub_rules)
-
-    def assign_identities(self):
-        identities = {character: "普通人" for character in self.characters}
-        for role in self.secret_main_rule.roles:
-            character = random.choice(list(identities.keys()))
-            identities[character] = role.name
-        return identities
-
-    def assign_event_criminals(self):
-        criminals = list(self.characters)
-        random.shuffle(criminals)
-        assigned_criminals = {}
-        for day, event in self.scheduled_events.items():
-            criminal = criminals.pop()
-            assigned_criminals[day] = criminal
-        return assigned_criminals
-
-    def get_public_info(self):
-        return {
-            "main_rule_table": self.main_rule_table.name,
-            "total_days": self.total_days,
-            "total_cycles": self.total_cycles,
-            "characters": [character.name for character in self.characters],
-            "scheduled_events": {day: event.name for day, event in self.scheduled_events.items()}
-        }
-
-    def get_secret_info(self):
-        return {
-            "secret_main_rule": self.secret_main_rule.name,
-            "secret_sub_rules": [rule.name for rule in self.secret_sub_rules],
-            "identities": self.identities,
-            "event_criminals": {day: criminal.name for day, criminal in self.event_criminals.items()}
-        }
+        random.shuffle
